2ce0da5bc0dfda55143db5e543e9090e80cf8780
sanie <bitakov73@mail.ru>
2024-09-28 23:18:06 +0300

    structers and algorithms

---
+++ practical_02/5a.cpp
@@ -0,0 +1,16 @@
+﻿// Задание 5(а)
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double w, x, y, z;
+
+	cin >> x >> y >> z;
+
+	w = sin(fabs((y - (sqrt(fabs(x)))) * (x - (y / ((pow(z, 2) + ((pow(x, 2) )/ 4))))) ));
+	cout << w;
+
+	return 0;
+}

5b92fbadd766afaf4a615d34e363ef7bdc161667
sanie <bitakov73@mail.ru>
2024-09-29 12:51:40 +0300

    Добавлен файл 11e.cpp

---
+++ practical_02/11e.cpp
@@ -0,0 +1,20 @@
+﻿// Задание 11(e)
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	double x, y;
+	cin >> x >> y;
+
+	if (x >= y && y >= -0.45 && x <= 1)
+	{
+		cout << "Точка попадает внутрь заданной области" << endl;
+	}
+	else
+	{
+		cout << "Точка не попадает внутрь заданной области" << endl;
+	}
+	return 0;
+}
